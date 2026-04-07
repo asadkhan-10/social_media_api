a5aee15deda4956a9c3cccfaeae2678f57351ba0
Asad ali khan <asad.khan.connect1@gmail.com>
2026-04-07 08:48:46 +0500

    final changes

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from .config import settings
 from fastapi.middleware.cors import CORSMiddleware
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 origins = ["http://localhost", "http://localhost:8080", "https://www.google.com"]
